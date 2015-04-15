6ce0dd9c3ae4d475a0a5a7870d5703a7d1693b9e
Édouard Touraile <edouard.touraille@gmail.com>
2015-04-15 16:25:42 +0200

    Evolution: ( Integration of facebook Events ) first step in integration add facebook login button add method to retrievr a user event not possible with graph api to get event not accepted

---
+++ client/config/config.js
@@ -0,0 +1,6 @@
+Accounts.ui.config({
+        requestPermissions: {
+                    facebook: [ 'user_events', 
+            'friends_events']
+    }
+});
--- client/templates/header.js
+++ client/templates/header.js
@@ -1,7 +1,8 @@
-
-
 Template.header.events({
-
-
-
+        'click #btn-user-events': function(e) {
+                    Meteor.call('getUserEvents', function(err, data) {
+                       // $('#result').text(JSON.stringify(data, undefined, 4));
+                        console.log('facebook-events',data);
+                    });
+         }
 });
